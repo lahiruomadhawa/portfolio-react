2f782b14d45d0ce68dbdbb472f06e11067f4c5d5
DESKTOP-VL7Q3DC\LahiruMadhawa <lahirumadhawa00@gmail.com>
2024-03-20 18:48:02 +1100

    Create new src files and required assets and components

---
+++ frontend_react/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+//import { About } from "./components/container";
+
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
+import "./App.scss";
+
+const App = () => {
+  //return <div>Adoo from App</div>;
+
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer /> 89 9890 `-`
+    </div>
+  );
+};
+
+export default App;
