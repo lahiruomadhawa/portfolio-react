bc6c2ff583b558dbedcce61549e07a9038d97938
DESKTOP-VL7Q3DC\LahiruMadhawa <lahirumadhawa00@gmail.com>
2024-03-24 12:54:46 +1100

    Social media links and profile details changed

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { FaFacebookF, FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
+      <a href='https://www.linkedin.com/in/lahiru-madhawamaha' target="_blank">
+        <FaLinkedin />
+      </a>
+    </div>
+    <div>
+      <a href='https://github.com/lahiruomadhawa' target="_blank">
+        <FaGithub />
+      </a>
+    </div>
+    <div>
       <a href='https://www.facebook.com/lahiru.ashan.9' target="_blank">
         <FaFacebookF />
-      </a>
-    </div>
-    <div>
-      <a href='https://www.instagram.com/lahiru.madhawa/' target="_blank">
-        <BsInstagram />
-      </a>
-    </div>
-    <div>
-      <a>
-        <BsTwitter />
       </a>
     </div>
   </div>
